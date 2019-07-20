53734492634d10fa446d99adb73f395928a7397e
Paris Cornett <pariscornett@gmail.com>
2019-07-20 10:45:52 -0500

    added routes

--- server.js
+++ server.js
@@ -19,3 +19,15 @@ app.use(express.json());
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
